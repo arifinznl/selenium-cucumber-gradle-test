4c78a0eff322bd33ead7ed4f3061b089d87a98dc
Zaenal Arifin <115356208+arifinznl@users.noreply.github.com>
2025-11-07 14:26:24 +0700

    Add Allure

--- src/test/java/com/zaenal/CucumberTest.java
+++ src/test/java/com/zaenal/CucumberTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources"},
         plugin = {"pretty",
                   "html:reports/cucumber.html", 
-                  "json:reports/cucumber.json"
+                  "json:reports/cucumber.json",
                  "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
                  }
 
